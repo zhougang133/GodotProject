10a0718d43b074927cd5be6f5961c18ce3ab43aa
zhougang <821528350@qq.com>
2026-02-08 15:11:10 +0800

    拖拽功能——待完善

---
+++ 网格放置/house.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+class_name House
+
+@export var rect: Rect2
+
+func get_global_rect():
+	return Rect2(
+		global_position - rect.size / 2,
+		rect.size
+	)
+	
+func set_on_place():
+	modulate.a = 1
