347380aa342993174ce622494ee6de4c8f764d84
zhougang133 <821528350@qq.com>
2026-02-24 20:28:54 +0800

    删除

--- 网格放置/grid_cell.gd
+++
@@ -1,8 +0,0 @@
-extends PanelContainer
-
-@export var full = false
-
-func change_color(color:Color):
-	var styleBox := get_theme_stylebox("panel").duplicate()
-	styleBox.bg_color = color
-	add_theme_stylebox_override("panel",styleBox)
